7e962d9264f24b00220d16299b771ac3c8291e3d
Shreya Sahni <shreyasahni7@gmail.com>
2020-08-16 20:32:25 +0530

    Update redirect.php

--- login page/redirect.php
+++ login page/redirect.php
@@ -1,26 +1,20 @@
 <?php 
 
 switch ($_POST['id']) {
-  case "admin":
-    header("Location:../IT/IT.html"); 
-    break;
   case "it":
-    header("Location:../IT/IT.html"); 
+    header("Location:../IT/IT.php"); 
     break;
   case "operations":
-    header("Location:../t_audit/t_audit_graph.html"); 
+    header("Location:../t_audit/t_audit_operations.php"); 
     break;
   case "audit":
-    header("Location:../t_audit/t_audit_graph.html"); 
+    header("Location:../t_audit/t_audit_graph.php"); 
     break;
   case "hr":
-    header("Location:../HR/HR.html"); 
+    header("Location:../HR/HR.php"); 
     break;
   case "hod":
-    header("Location:../HOD-1/hod.html"); 
-    break;
-  case "enc":
-    header("Location: http://www.geeksforgeeks.org"); 
+    header("Location:../HOD-1/hod.php"); 
     break;
   default:
     echo "Wrong input"; 
